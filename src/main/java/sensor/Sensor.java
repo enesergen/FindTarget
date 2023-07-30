29b176d9ebe6baf8cc8c602d80867d8373a11e58
Enes Ergen <eenes44@gmail.com>
2023-07-30 18:41:00 +0300

    Update view

--- src/main/java/sensor/Sensor.java
+++ src/main/java/sensor/Sensor.java
@@ -1,2 +1,80 @@
-package sensor;public class Sensor {
+package sensor;
+
+import process.Processer;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class Sensor implements Serializable {
+    int[] coordinates;
+    String sensorName;
+
+    Sensor(String sensorName) {
+        Random random = new Random();
+        coordinates =
+                new int[]
+                        {
+                                -500 + random.nextInt(1000),
+                                -500 + random.nextInt(1000)
+                        };
+        this.sensorName = sensorName;
+    }
+
+
+    public void createServer(int port) {
+
+        try (ServerSocket server = new ServerSocket(port)) {
+            server.setReuseAddress(true);
+            while (true) {
+                Socket target = server.accept();
+                target.setKeepAlive(true);
+                System.out.println("Target Connected" + target.getInetAddress().getHostAddress());
+                TargetHandler targetHandler = new TargetHandler(target, this);
+                new Thread(targetHandler).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private record TargetHandler(Socket socket, Sensor sensor) implements Runnable {
+
+        @Override
+        public void run() {
+            try (
+                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream())
+            ) {
+                while (true) {
+                    int[] receivedCoordinatesFromTarget = (int[]) ois.readObject();
+                    System.out.printf
+                            ("Sensor coordinates:(%d,%d) | Target coordinates:(%d,%d)%n",
+                                    sensor.coordinates[0],
+                                    sensor.coordinates[1],
+                                    receivedCoordinatesFromTarget[0],
+                                    receivedCoordinatesFromTarget[1]);
+                    String message = "Coordinates was taken successfully by " + sensor.sensorName;
+                    Processer.processUnit(sensor.sensorName, sensor.coordinates, receivedCoordinatesFromTarget);
+                    oos.writeObject(message);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Sensor sensor1 = new Sensor("Sensor-1");
+        new Thread(() -> {
+            sensor1.createServer(8080);
+        }).start();
+        Sensor sensor2 = new Sensor("Sensor-2");
+        new Thread(() -> {
+            sensor2.createServer(8081);
+        }).start();
+
+    }
 }
--- src/main/java/target/Target.java
+++ src/main/java/target/Target.java
@@ -1,2 +1,80 @@
-package target;public class Target {
+package target;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Random;
+
+public class Target {
+    int[] coordinates;
+
+    Target() {
+        Random random = new Random();
+        coordinates =
+                new int[]
+                        {
+                                -500 + random.nextInt(1000),
+                                -500 + random.nextInt(1000)
+                        };
+    }
+
+
+    public void sendCoordinates(int port) {
+        boolean isConnected = false;
+        Socket socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        while (!isConnected) {
+            try {
+                socket = new Socket("localhost", port);
+                oos = new ObjectOutputStream(socket.getOutputStream());
+                ois = new ObjectInputStream(socket.getInputStream());
+                isConnected = true;
+                System.out.println("Connection is provided for port:" + port);
+            } catch (IOException e) {
+                System.out.println("Try to connect sensor");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+        while (true) {
+            try {
+                if (isConnected) {
+                    oos.writeObject(coordinates);
+                    System.out.println("Sensor:" + (String) ois.readObject());
+                    Thread.sleep(1000);
+                } else {
+                    socket = new Socket("localhost", port);
+                    oos = new ObjectOutputStream(socket.getOutputStream());
+                    ois = new ObjectInputStream(socket.getInputStream());
+                    isConnected = true;
+                    Thread.sleep(1000);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                isConnected = false;
+                System.out.println("Connection failed. It will try to provide connection");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Target target = new Target();
+        new Thread(() -> {
+            target.sendCoordinates(8080);
+        }).start();
+        new Thread(() -> {
+            target.sendCoordinates(8081);
+        }).start();
+    }
 }
